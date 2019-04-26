d9acf3732420cd313aff4dd3acd1672da38d713d
Chris McKenzie <kristopolous@yahoo.com>
2019-04-26 16:02:06 -0700

    trying to construct who is charging from postback info

--- session.php
+++ session.php
@@ -2,22 +2,26 @@
 include('lib.php');
 $data = json_decode(file_get_contents('php://input'), true);
 
+$evse = aget($data, 'location.id');
 $row = db_get($data['id']);
+$user = reservation($evse);
+
 if(!$row) {
   $row = ['id' => db_insert('sessions', [
       'start' 	=> aget($data, 'start_datetime'),
       'session' => aget($data, 'id'),
-      'evse' 		=> aget($data, 'location.id'),
+      'user'    => $user,
+      'evse' 		=> $evse,
       'status' 	=> aget($data, 'status'),
     ])
   ];
 } 
 
 db_update('sessions', $row['id'], [
-  'end' => aget($data, 'end_datetime'),
-  'kwh' => aget($data, 'kwh'),
-  'cost' => aget($data, 'total_cost'),
-  'status' => aget($data, 'status')
+  'end'     => aget($data, 'end_datetime'),
+  'kwh'     => aget($data, 'kwh'),
+  'cost'    => aget($data, 'total_cost'),
+  'status'  => aget($data, 'status')
 ]);
 
 file_put_contents(
