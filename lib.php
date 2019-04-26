6f712b4e6e7036643a03885809b50099c132356d
Chris McKenzie <kristopolous@yahoo.com>
2019-04-26 15:01:52 -0700

    bringing over more library

--- lib.php
+++ lib.php
@@ -13,3 +13,39 @@ $schema = [
   ]
 ];
 
+function get_column_list($table_name) {
+  $db = db_connect();
+  $res = $db->query("pragma table_info( $table_name )");
+
+  return array_map(function($row) { 
+    return $row['name'];
+  }, sql_all($res));
+}
+
+function db_connect() {
+  global $db;
+  return $db;
+}
+
+function sql_all($sql_res) {
+  $res = [];
+  while( ($res[] = $sql_res->fetchArray(SQLITE3_ASSOC)) );
+  array_pop($res);
+  return $res;
+}
+
+function sql_kv($hash, $operator = '=', $quotes = "'", $intList = []) {
+  $ret = [];
+  foreach($hash as $key => $value) {
+    if ( is_string($value) ) {
+      if(in_array($key, $intList)) {
+        $ret[] = "$key $operator $value";
+      } else {
+        $ret[] = "$key $operator $quotes$value$quotes";
+      }
+    }
+  } 
+  return $ret;
+}
+
+
