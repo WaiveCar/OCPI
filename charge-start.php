0eeb41da40764eb115ba82df8cb2c6705bbb223f
Chris McKenzie <kristopolous@yahoo.com>
2019-04-26 16:01:21 -0700

    User inferencing from the charger start

--- charge-start.php
+++ charge-start.php
@@ -1,7 +1,8 @@
 <?php
-$data = file_get_contents('php://input');
-//file_put_contents("/tmp/session.tmp", json_encode($data) . "\n", FILE_APPEND);
-if(!empty($data)) {
-  file_put_contents("/tmp/session-post.tmp", $data . "\n", FILE_APPEND);
-}
-//file_put_contents("/tmp/session.tmp", json_encode([$_REQUEST, $_SERVER, getallheaders()]) . "\n", FILE_APPEND);
+include('lib.php');
+
+reservation(
+  aget($_GET, 'evse'), 
+  aget($_GET, 'user')
+);
+
