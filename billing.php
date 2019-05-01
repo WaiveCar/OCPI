70cd938789461c8cdac3733d6c019d259cdc7bb0
Chris McKenzie <kristopolous@yahoo.com>
2019-05-01 15:00:24 -0700

    fixing tabbing

--- billing.php
+++ billing.php
@@ -6,28 +6,41 @@ if(aget($_GET, 'key') !== $key) {
   return do_error('Bad key');
 }
 
+$append = false;
+$user = intval(aget($_GET, 'user'));
+if($user) {
+  $append = "and user = $user";
+}
+
 $verb = $_SERVER['REQUEST_METHOD'];
 if($verb === 'GET') {
-  $append = false;
-  $user = intval(aget($_GET, 'user'));
-  if($user) {
-    $append = "and user = $user";
-  }
-
-	return do_success(db_all("select * from sessions where paid='false' $append"));
+  return do_success(db_all("select * from sessions where status='COMPLETED' and paid='false' $append"));
 }
 
 if($verb === 'POST' || $verb === 'PUT') {
-	$raw = file_get_contents('php://input');
-	$data = json_decode($raw, true);
-	if($data) {
-		// filter out non-numeric
-		$data = array_map(function($row) { return intval($row);}, $data);
-		$str = implode(',', $data);
+  $raw = file_get_contents('php://input');
+  $data = json_decode($raw, true);
+  if($data) {
+    // filter out non-numeric
+    $data = array_map(function($row) { return intval($row); }, $data);
+    $str = implode(',', $data);
     $res = db_one("update sessions set paid='true' where id in ($str)");
-    do_success($res);
+    do_success($data);
   } else {
     do_error('Bad Data');
   }
+}
+
+if($verb === 'DELETE') {
+  $data = aget($_GET, 'id');
+  if($data) {
+    $data = explode(',', $data);
+    $data = array_map(function($row) { return intval($row); }, $data);
+    $str = implode(',', $data);
+    $append .= " and id in ($str)";
+  }
+
+  $res = db_one("update sessions set paid='false' where paid='true' $append");
+  do_success($data);
 }
 
