ba311843867d561ce35204ca0d1be5f911e27114
Chris McKenzie <kristopolous@yahoo.com>
2019-06-05 17:05:27 -0700

    Updating the tool to fill in the gaps of missing costs in the table

--- fill-in-gaps.php
+++ fill-in-gaps.php
@@ -1,3 +1,4 @@
+#!/usr/bin/php
 <?php
 
 include('lib.php');
@@ -43,12 +44,38 @@ function curldo($url, $params = false, $opts = []) {
   return $res;
 }
 
-$earliest = db_one('select min(start) from sessions where cost is null');
-var_dump($earliest);exit;
-$res = curldo('sessions', ['date_from' => '2019-05-28T22:35:04Z']);
+# We don't really want to worry about timezones 
+# nor do we want to get into a nasty loop so we 
+# give a nice baseline that's not too far back 
+# in time that it may give us some edge condition
+# issues but also far back enough so that every session
+# we want to see we do see!
+$earliest = db_one('select datetime(min(start), "-18 hour") as m from sessions where cost is null');
 
+if(empty($earliest)) {
+  echo "Nothing to do!"
+  exit(0);
+}
+
+#
+# sqlite doesn't seem to give us the ISO format with
+# the T and Z thing (see our notion of timestamps from
+# above --- where I objectively just don't give a shit)
+#
+# so let's fix that here.
+#
+$earliest = str_replace(' ', 'T', $earliest['m']) . 'Z';
+
+$emptyList = array_map(function($row) { return $row['session']; }, db_all('select session from sessions where cost is null order by id asc'));
+
+$res = curldo('sessions', ['date_from' => $earliest]);
+
+$db = get_db();
 if (!empty($res['data'])) {
   foreach($res['data'] as $session) {
-    echo "${session['id']} ${session['kwh']} ${session['total_cost']}\n";
+    if (array_search($session['id'], $emptyList) !== false) {
+      $qstr = "update sessions set status='${session['status']}', cost=${session['total_cost']} where session=${session['id']}\n";
+      $db->exec($qstr);
+    }
   }
 }
